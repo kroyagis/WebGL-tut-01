0aa2b3c794e6ca0ad89b8a416573cc3ea276f5e4
Ji You <kroyagis@gmail.com>
2016-08-30 11:45:00 -0400

    -initialized InitDemo() in app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var InitDemo = function(){
-  console.log("This is Working");
+  console.log("This is working");
+
+  var canvas = document.getElementById('game-surface');
+  var gl = canvas.getContext('webgl');
+
+  if (!gl) {
+    console.log('WebGL not supported, falling back on experimental-webgl');
+    gl = canvas.getContext('experimental-webgl');
+  }
+
+  if (!gl){
+    alert('Your browser does not support WebGL');
+  }
 };
